9d5b0780b4b4a5a1073cb7660b8631eb3e48a14b
Shahbaz Sharif <shahbaz.sharif@leopardscourier.com>
2024-04-04 16:04:30 +0500

    asat

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -16,216 +16,218 @@ class HomeScreen extends StatelessWidget {
         width: MediaQuery.of(context).size.width,
         child: Padding(
           padding: const EdgeInsets.all(5.0),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 100,
-                width: MediaQuery.of(context).size.width - 80,
-                child: TextField(
-                  style: const TextStyle(
-                    color: Color(0xff020202),
-                    fontSize: 18,
-                    fontWeight: FontWeight.w400,
-                    letterSpacing: 0.5,
-                  ),
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: const Color(0xfff1f1f1),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: BorderSide.none,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 100,
+                  width: MediaQuery.of(context).size.width - 80,
+                  child: TextField(
+                    style: const TextStyle(
+                      color: Color(0xff020202),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                      letterSpacing: 0.5,
                     ),
-                    hintText: "Search",
-                    hintStyle: const TextStyle(
-                        color:  Color(0xffb2b2b2),
-                        fontSize: 18,
-                        fontWeight: FontWeight.w400,
-                        letterSpacing: 0.5,
-                        decorationThickness: 6),
-                    prefixIcon: const Icon(Icons.search),
-                    prefixIconColor: Colors.black,
-                  ),
-                ),
-              ),
-              Container(
-                height: 200,
-                width: MediaQuery.of(context).size.width - 80,
-                decoration:  BoxDecoration(
-                  color: const Color(0xff1c2e4a),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Row(
-                    children: [
-                      const Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Name", style: TextStyle(
-                              color: Color(0xffffad03),
-                              fontSize: 18
-                          ),),
-                          Text("Email", style: TextStyle(
-                              color: Color(0xffffad03),
-                              fontSize: 18
-                          ),),
-                          Text("Phone Number", style: TextStyle(
-                              color: Color(0xffffad03),
-                              fontSize: 18
-                          ),),
-                        ],
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color(0xfff1f1f1),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide.none,
                       ),
-                      const SizedBox(
-                        width: 60,
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width * (1/3),
-                        decoration:  BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(20),
-                          image: const DecorationImage(
-                            image: AssetImage('assets/images/profile.png'),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              Container(
-                height: 120,
-                width: MediaQuery.of(context).size.width - 80,
-                decoration:  BoxDecoration(
-                  color: const Color(0xff1c2e4a),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container(
-                        width: 100,
-                        decoration:  BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                          image: const DecorationImage(
-                            image: AssetImage('assets/images/contract.png'),
-                            fit: BoxFit.fill,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 20,
-                      ),
-                      const Text("Marrige Contracts", style: TextStyle(
-                          color: Color(0xffffad03),
-                          fontSize: 18
-                      ),)
-                    ],
-                  ),
-                ),
-              ),
-
-              const SizedBox(
-                height: 40,
-              ),
-              Container(
-                height: 120,
-                width: MediaQuery.of(context).size.width - 80,
-                decoration:  BoxDecoration(
-                  color: const Color(0xff1c2e4a),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container(
-                        width: 100,
-                        decoration:  BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                          image: const DecorationImage(
-                            image: AssetImage('assets/images/lawyer.png'),
-                            fit: BoxFit.fill,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 20,
-                      ),
-                      const Text("Divorces", style: TextStyle(
-                          color: Color(0xffffad03),
-                          fontSize: 18
-                      ),)
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              Container(
-                height: 120,
-                width: MediaQuery.of(context).size.width - 80,
-                decoration:  BoxDecoration(
-                  color: const Color(0xff1c2e4a),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container(
-                        width: 100,
-                        decoration:  BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                          image: const DecorationImage(
-                            image: AssetImage('assets/images/attorney.png'),
-                            fit: BoxFit.fill,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 20,
-                      ),
-                      const Text("Power of Attorney", style: TextStyle(
-                          color: Color(0xffffad03),
-                          fontSize: 18
-                      ),)
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(
-                width: 20,
-              ),
-              Center(
-                child: SizedBox(
-                  width: 200, // specify the desired width
-                  height: 100, // specify the desired height
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamed('/PaymentScreen');
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(Colors.yellow),
-                    ),
-                    child: Text(
-                      'Pay Now',
-                      style: TextStyle(fontSize: 20), // increase font size if needed
+                      hintText: "Search",
+                      hintStyle: const TextStyle(
+                          color:  Color(0xffb2b2b2),
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 0.5,
+                          decorationThickness: 6),
+                      prefixIcon: const Icon(Icons.search),
+                      prefixIconColor: Colors.black,
                     ),
                   ),
                 ),
-              )
-,
-            ],
+                Container(
+                  height: 200,
+                  width: MediaQuery.of(context).size.width - 80,
+                  decoration:  BoxDecoration(
+                    color: const Color(0xff1c2e4a),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: [
+                        const Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Name", style: TextStyle(
+                                color: Color(0xffffad03),
+                                fontSize: 18
+                            ),),
+                            Text("Email", style: TextStyle(
+                                color: Color(0xffffad03),
+                                fontSize: 18
+                            ),),
+                            Text("Phone Number", style: TextStyle(
+                                color: Color(0xffffad03),
+                                fontSize: 18
+                            ),),
+                          ],
+                        ),
+                        const SizedBox(
+                          width: 60,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * (1/3),
+                          decoration:  BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                            image: const DecorationImage(
+                              image: AssetImage('assets/images/profile.png'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  height: 120,
+                  width: MediaQuery.of(context).size.width - 80,
+                  decoration:  BoxDecoration(
+                    color: const Color(0xff1c2e4a),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 100,
+                          decoration:  BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            image: const DecorationImage(
+                              image: AssetImage('assets/images/contract.png'),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        const Text("Marrige Contracts", style: TextStyle(
+                            color: Color(0xffffad03),
+                            fontSize: 18
+                        ),)
+                      ],
+                    ),
+                  ),
+                ),
+            
+                const SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  height: 120,
+                  width: MediaQuery.of(context).size.width - 80,
+                  decoration:  BoxDecoration(
+                    color: const Color(0xff1c2e4a),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 100,
+                          decoration:  BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            image: const DecorationImage(
+                              image: AssetImage('assets/images/divorce.png'),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        const Text("Divorces", style: TextStyle(
+                            color: Color(0xffffad03),
+                            fontSize: 18
+                        ),)
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  height: 120,
+                  width: MediaQuery.of(context).size.width - 80,
+                  decoration:  BoxDecoration(
+                    color: const Color(0xff1c2e4a),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 100,
+                          decoration:  BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            image: const DecorationImage(
+                              image: AssetImage('assets/images/attorney.png'),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        const Text("Power of Attorney", style: TextStyle(
+                            color: Color(0xffffad03),
+                            fontSize: 18
+                        ),)
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Center(
+                  child: SizedBox(
+                    width: 200, // specify the desired width
+                    height: 100, // specify the desired height
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pushNamed('/PaymentScreen');
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(Colors.yellow),
+                      ),
+                      child: Text(
+                        'Pay Now',
+                        style: TextStyle(fontSize: 20), // increase font size if needed
+                      ),
+                    ),
+                  ),
+                )
+            ,
+              ],
+            ),
           ),
         ),
       ),
--- lib/PaymentScreen.dart
+++ lib/PaymentScreen.dart
@@ -19,114 +19,118 @@ class _PaymentScreenState extends State<PaymentScreen> {
       ),
       body: Container(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 40,
-            ),
-            Container(
-              height: 120,
-              width: MediaQuery.of(context).size.width - 80,
-              decoration:  BoxDecoration(
-                color: const Color(0xff1c2e4a),
-                borderRadius: BorderRadius.circular(20),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 40,
               ),
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    const Text("Total Price", textAlign: TextAlign.start,
-                      style: TextStyle(
+              Container(
+                height: 120,
+                width: MediaQuery.of(context).size.width - 80,
+                decoration:  BoxDecoration(
+                  color: const Color(0xff1c2e4a),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const Text("Total Price", textAlign: TextAlign.start,
+                        style: TextStyle(
+                            color: Color(0xFFFFFFFF),
+                          fontSize: 18
+                      ),
+          
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                       Text("2311", style: TextStyle(
                           color: Color(0xFFFFFFFF),
-                        fontSize: 18
-                    ),
-
-                    ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                     Text("2311", style: TextStyle(
-                        color: Color(0xFFFFFFFF),
-                        fontSize: 18
-                    ),)
-                  ],
+                          fontSize: 18
+                      ),)
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Container(
-              height: 80,
-              width: 850,
-              decoration:  BoxDecoration(
-                image: const DecorationImage(
-                  image: AssetImage("assets/images/img.png"),
+              Container(
+                height: 80,
+                width: 850,
+                decoration:  BoxDecoration(
+                  image: const DecorationImage(
+                    image: AssetImage("assets/images/img.png"),
+                  ),
                 ),
               ),
-            ),
-            Text(
-              'Credit Card Number',
-              style: TextStyle(fontSize: 16),
-            ),
-            TextFormField(
-              controller: _cardController,
-              keyboardType: TextInputType.number,
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Card Holder Name',
-              style: TextStyle(fontSize: 16),
-            ),
-            TextFormField(
-              controller: _expiryController,
-              keyboardType: TextInputType.text,
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Amount',
-              style: TextStyle(fontSize: 16),
-            ),
-            TextFormField(
-              controller: _cvcController,
-              keyboardType: TextInputType.number,
-            ),
-            SizedBox(height: 32),
-
-
-            Center(
-              child: SizedBox(
-                width: 600, // specify the desired width
-                height: 80, // specify the desired height
-                child: ElevatedButton(
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: Text('PAYMENT'),
-                          content: Text('PAYMENT HAS BEEN PAID'),
-                          actions: <Widget>[
-                            TextButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                              child: Text('OK'),
-                            ),
-                          ],
-                        );
-                      },
-                    );
-
-                  },   style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.yellow),
-                ),
-                  child: Text('Pay Now'),
+              Text(
+                'Credit Card Number',
+                style: TextStyle(fontSize: 16),
+              ),
+              TextFormField(
+                controller: _cardController,
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Card Holder Name',
+                style: TextStyle(fontSize: 16),
+              ),
+              TextFormField(
+                controller: _expiryController,
+                keyboardType: TextInputType.text,
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Amount',
+                style: TextStyle(fontSize: 16),
+              ),
+              TextFormField(
+                controller: _cvcController,
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(height: 32),
+              const SizedBox(
+                width: 20,
+              ),
+          
+              Center(
+                child: SizedBox(
+                  width: 600, // specify the desired width
+                  height: 80, // specify the desired height
+                  child: ElevatedButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('PAYMENT'),
+                            content: Text('PAYMENT HAS BEEN PAID'),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('OK'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+          
+                    },   style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.yellow),
+                  ),
+                    child: Text('Pay Now'),
+                  ),
                 ),
               ),
-            ),
-
-
-          ],
+          
+          
+            ],
+          ),
         ),
       ),
     );
