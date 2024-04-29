45bb2838bd0e45967fb1285eaee7c3a0f77ea173
Shahbaz Sharif <shahbaz.sharif@leopardscourier.com>
2024-04-29 18:49:53 +0500

    infromationuser

--- lib/InformationUser.dart
+++ lib/InformationUser.dart
@@ -3,17 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class InformationUser extends StatelessWidget {
+  String contract = " ";
+  String price = " ";
 
-  String contract=" ";
-  String price=" ";
   @override
   Widget build(BuildContext context) {
-    final Map<String, dynamic>? args = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>?;
-
+    final Map<String, dynamic>? args =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>?;
 
     if (args != null) {
       contract = args['contract'];
-      price=args['price'];
+      price = args['price'];
     }
 
     return MaterialApp(
@@ -21,41 +21,47 @@ class InformationUser extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: UserInfromationScreen(price: price,contract: contract,),
+      home: UserInfromationScreen(
+        price: price,
+        contract: contract,
+      ),
     );
   }
 }
-
 
 class UserInfromationScreen extends StatefulWidget {
   final String price;
   final String contract;
 
-
-  UserInfromationScreen({required this.price,required this.contract });
+  UserInfromationScreen({required this.price, required this.contract});
 
   @override
-  _UserInfromationState createState() => _UserInfromationState(price: price,contract: contract);
-
+  _UserInfromationState createState() =>
+      _UserInfromationState(price: price, contract: contract);
 }
 
 class _UserInfromationState extends State<UserInfromationScreen> {
   final String price;
   final String contract;
-  _UserInfromationState({required this.price,required this.contract });
+
+  _UserInfromationState({required this.price, required this.contract});
+
   late TextEditingController _dateController;
   final TextEditingController fullnameController = new TextEditingController();
   final TextEditingController pdffileController = new TextEditingController();
-  final TextEditingController phonenumberController = new TextEditingController();
+  final TextEditingController phonenumberController =
+      new TextEditingController();
   final _formKey = GlobalKey<FormState>();
+
   @override
   void initState() {
     super.initState();
     // Set the initial value of the date controller to the current date
-    _dateController = TextEditingController(text: DateFormat('yyyy-MM-dd').format(DateTime.now()));
+    _dateController = TextEditingController(
+        text: DateFormat('yyyy-MM-dd').format(DateTime.now()));
   }
-  // Function to show the date picker dialog
 
+  // Function to show the date picker dialog
 
   Future<void> _selectDate(BuildContext context) async {
     final DateTime? picked = await showDatePicker(
@@ -74,16 +80,12 @@ class _UserInfromationState extends State<UserInfromationScreen> {
 
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
             image: DecorationImage(
                 image: AssetImage('assets/images/background.png'),
-                fit: BoxFit.fill
-            )
-        ),
+                fit: BoxFit.fill)),
         child: Center(
           child: Form(
             key: _formKey,
@@ -91,28 +93,22 @@ class _UserInfromationState extends State<UserInfromationScreen> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 TextFormField(
-                  controller: fullnameController,
-                  cursorColor: Colors.white,
-                  style: const TextStyle(color: Colors.black),
-                  decoration: const InputDecoration(
-                    icon: Icon(Icons.person, color: Colors.black),
-                    hintText: "fullname",
-                    border: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black)),
-                    hintStyle: TextStyle(color: Colors.black),
-                  ),
-
+                    controller: fullnameController,
+                    cursorColor: Colors.white,
+                    style: const TextStyle(color: Colors.black),
+                    decoration: const InputDecoration(
+                      icon: Icon(Icons.person, color: Colors.black),
+                      hintText: "fullname",
+                      border: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black)),
+                      hintStyle: TextStyle(color: Colors.black),
+                    ),
                     validator: (value) {
-                if (value == null || value.isEmpty) {
-                return 'Please enter your fullname';
-
-                 }
-                  }
-
-                ),
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your fullname';
+                      }
+                    }),
                 const SizedBox(height: 30.0),
-
-
                 TextFormField(
                   controller: _dateController,
                   cursorColor: Colors.white,
@@ -133,10 +129,11 @@ class _UserInfromationState extends State<UserInfromationScreen> {
                       return 'Please enter your Born Date';
                     }
                   },
-                  readOnly: true, // So the user has to use the calendar to pick the date
+                  readOnly:
+                      true, // So the user has to use the calendar to pick the date
                 ),
                 TextFormField(
-                    controller: phonenumberController,
+                  controller: phonenumberController,
                   cursorColor: Colors.white,
                   style: const TextStyle(color: Colors.black),
                   keyboardType: TextInputType.number,
@@ -153,37 +150,26 @@ class _UserInfromationState extends State<UserInfromationScreen> {
                     }
                     if (value.length < 9) {
                       return 'please enter valid phone number';
-
                     }
                     return null;
                   },
-
-
                 ),
-
                 TextFormField(
-                  //  controller: passwordController,
-                  cursorColor: Colors.white,
-                  style: const TextStyle(color: Colors.black),
-                  decoration: const InputDecoration(
-                    icon: Icon(Icons.lock, color: Colors.black),
-                    hintText: "pdf file link",
-                    border: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black)),
-                    hintStyle: TextStyle(color: Colors.black),
-                  ),
-
-    validator: (value) {
-    if (value == null || value.isEmpty) {
-    return 'Please enter valid link';
-    }
-                  }
-
-
-
-                ),
-
-
+                    //  controller: passwordController,
+                    cursorColor: Colors.white,
+                    style: const TextStyle(color: Colors.black),
+                    decoration: const InputDecoration(
+                      icon: Icon(Icons.lock, color: Colors.black),
+                      hintText: "pdf file link",
+                      border: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black)),
+                      hintStyle: TextStyle(color: Colors.black),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter valid link';
+                      }
+                    }),
                 Container(
                   width: MediaQuery.of(context).size.width,
                   height: 40.0,
@@ -191,29 +177,27 @@ class _UserInfromationState extends State<UserInfromationScreen> {
                   margin: const EdgeInsets.only(top: 15.0),
                   child: ElevatedButton(
                     onPressed: () {
-
-
                       if (_formKey.currentState?.validate() == true) {
                         ScaffoldMessenger.of(context).showSnackBar(
                           SnackBar(content: Text('Processing Data')),
                         );
 
-
-
                         showDialog(
                           context: context,
                           builder: (BuildContext context) {
                             return AlertDialog(
-                              title: Text('$contract'),
-                              content: Text('$contract  has been Register with the with payment of $price'),
+                              title: Text(widget.contract),
+                              content: Text(
+                                  '$contract  has been Register with the with payment of $price'),
                               actions: <Widget>[
                                 TextButton(
                                   onPressed: () {
                                     Navigator.of(context).pop();
-                                    Navigator.of(context).pushNamed('/PaymentScreen',arguments: {
-                                      'price': "$price",
-                                    });
-
+                                    Navigator.of(context).pushNamed(
+                                        '/PaymentScreen',
+                                        arguments: {
+                                          'price': "${widget.price}",
+                                        });
                                   },
                                   child: Text('OK'),
                                 ),
@@ -221,24 +205,12 @@ class _UserInfromationState extends State<UserInfromationScreen> {
                             );
                           },
                         );
-
-
-
                       }
-
-
-
                     },
                     child: const Text("Submit",
                         style: TextStyle(color: Colors.black)),
                   ),
                 ),
-
-
-
-
-
-
               ],
             ),
           ),
